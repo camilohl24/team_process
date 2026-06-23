e000501e68779fcb2bfbb9e5fe136dc42fb70a1b
camilohl24 <juancamilohernandezhl@gmail.com>
2026-06-23 18:31:25 -0500

    feat: connection with data base ready

---
+++ TeamProcess.API/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeamProcess.API.Models;
+
+namespace TeamProcess.API.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Attendance> Attendances { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Department> Departments { get; set; }
+}

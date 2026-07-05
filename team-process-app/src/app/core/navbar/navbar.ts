27cec24a393f6571d562b6303417e12a45739568
camilohl24 <juancamilohernandezhl@gmail.com>
2026-07-05 18:48:48 -0500

    feat: add navbar component with icons and styling.

--- team-process-app/src/app/core/navbar/navbar.ts
+++ team-process-app/src/app/core/navbar/navbar.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-
+import { LucideAngularModule,LayoutDashboard,Users,Building2,CalendarCheck } from 'lucide-angular';
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, LucideAngularModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  readonly layoutDashboard = LayoutDashboard;
+  readonly users = Users;
+  readonly building2 = Building2;
+  readonly calendarCheck = CalendarCheck;
+}

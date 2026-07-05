76b5662a1ad03c0c2a17021af1120eb58a3acdd4
camilohl24 <juancamilohernandezhl@gmail.com>
2026-07-04 21:01:30 -0500

    feat: add navbar and routes.

--- team-process-app/src/app/app.routes.ts
+++ team-process-app/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { DepartmentList } from './features/departments/department-list/department-list';
+import { EmployeeList } from './features/employees/employee-list/employee-list';
+import { AttendanceList } from './features/attendances/attendance-list/attendance-list';
+import { Dashboard } from './features/dashboard/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'departments', component: DepartmentList},
+    {path:'employees', component: EmployeeList},
+    {path:'attendances', component: AttendanceList},
+    {path:'dashboard', component: Dashboard},
+    {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+    {path: '**', redirectTo: 'dashboard'}
+];
